6cdb4991d02329b637b5ee7cdbae13d34b0b549b
Ronald Kwok <ronaldkwok1994@gmail.com>
2019-09-10 14:20:30 +0800

    modify model "MobileApp" structure and add update function

--- src/model/MobileApp.js
+++ src/model/MobileApp.js
@@ -1,14 +1,25 @@
 export default class MobileApp {
-    appID: BigInteger;
+    appID: Number;
     name: String;
+    summary: String;
+    artist: String;
     imageUrl: String;
     category: String;
+    starsNumber: Number;
+    ratingCount: Number;
 
     constructor(json) {
         this.appID = json.id.attributes['im:id'];
         this.name = json['im:name'].label;
+        this.summary = json.summary.label;
+        this.artist = json['im:artist'].label;
         this.imageUrl = json['im:image'][1].label;
         this.category = json.category.attributes.label;
+    }
+
+    updateRating(json) {
+        this.starsNumber = json.results[0].averageUserRating;
+        this.ratingCount = json.results[0].userRatingCount;
     }
 
     filter(searchText) {
